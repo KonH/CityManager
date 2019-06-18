ddb5af4c389b67348bf99d0ae1098a2a8eb3b34f
KonH <konh@yandex.ru>
2019-06-18 14:19:44 +0700

    Camera setup

---
+++ Assets/Scripts/Camera/DistanceHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CityManager.Camera {
+	public class DistanceHandler : MonoBehaviour {
+		public float Speed;
+
+		void LateUpdate() {
+			var axis = Input.GetAxis("Mouse ScrollWheel");
+			transform.localPosition += axis * Speed * Time.deltaTime * Vector3.forward;
+		}
+	}
+}
---
+++ Assets/Scripts/Camera/PositionHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CityManager.Camera {
+	public class PositionHandler : MonoBehaviour {
+		public float Speed;
+		
+		void LateUpdate() {
+			var z = Input.GetAxis("Vertical");
+			var x = Input.GetAxis("Horizontal");
+			var positionDelta = new Vector3(x, 0, z);
+			transform.localPosition += Time.deltaTime * Speed * positionDelta;
+		}
+	}
+}
